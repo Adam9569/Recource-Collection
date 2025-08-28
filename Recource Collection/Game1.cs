f2129e1fb39a84ee8342e101cb2f555ef48a94d6
adam szczepaniak <adamszczepaniak07@gmail.com>
2025-08-28 14:22:42 +0100

    made the boss SpriteSheet , animated the boss's idle position

--- Recource Collection/Game1.cs
+++ Recource Collection/Game1.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Diagnostics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Recource_Collection;
 
 namespace Recource_Collection
 {
     public class Game1 : Game
     {
+        private Matrix _translation;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        public Texture2D heroTexture;
+        private Hero _hero;
+        Texture2D spriteSheet;
+
+        int counter;
+        int activeFrame;
+        int numFrames;
+
 
         public Game1()
         {
@@ -16,9 +27,18 @@ namespace Recource_Collection
             IsMouseVisible = true;
         }
 
+        private void TranslationCalc()
+        {
+            _translation = Matrix.CreateTranslation(-_hero.Position.X + Globals.WindowSize.X / 2,- _hero.Position.Y + Globals.WindowSize.Y / 2,0f);
+
+        }
+
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            Globals.WindowSize = new(1024, 768);
+            _graphics.PreferredBackBufferWidth = Globals.WindowSize.X;
+            _graphics.PreferredBackBufferHeight = Globals.WindowSize.Y;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,25 +46,48 @@ namespace Recource_Collection
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            activeFrame = 0;
+            numFrames = 2;
+            counter = 0;
 
-            // TODO: use this.Content to load your game content here
+
+            spriteSheet = Content.Load<Texture2D>("treeantSpriteSheet1");
+            Globals.SpriteBatch = _spriteBatch;
+            heroTexture = Content.Load<Texture2D>("hero");
+            _hero = new Hero(heroTexture, new Vector2(100, 100));
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
+            Globals.Update(gameTime);
+            var keyboardState = Keyboard.GetState();
+            TranslationCalc();
+            InputManager.Update();
+            _hero.Update();
             base.Update(gameTime);
+
+            counter++;
+            if(counter > 29)
+            {
+                counter = 0;
+                activeFrame++;
+
+                if(activeFrame == numFrames)
+                {
+                    activeFrame = 0;
+                }
+            }
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin(transformMatrix: _translation);
+            _spriteBatch.Draw(spriteSheet, new Rectangle(100,100,200,200),new Rectangle((activeFrame*32),33,32,32),Color.White);
+            _hero.Draw();
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
