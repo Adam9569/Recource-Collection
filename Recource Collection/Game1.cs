517c8ee802567afa98ae7b9082bcde42f954312b
adam szczepaniak <adamszczepaniak07@gmail.com>
2025-10-16 10:53:02 +0100

    tried to make it so that the qestions can be checked agaist the input but it didnt work and doesnt display either.

--- Recource Collection/Game1.cs
+++ Recource Collection/Game1.cs
@@ -23,7 +23,7 @@ namespace Recource_Collection
 
         public KeyboardState currentState = Keyboard.GetState();
         public KeyboardState previousState;
-        public int CurrentQuestionIndex = 0;
+        
 
         public Texture2D heroTexture;
         private Hero _hero;
@@ -60,6 +60,8 @@ namespace Recource_Collection
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             Globals.SpriteBatch = _spriteBatch;
+            _questions = QuestionManager.LoadQuestions("Content/Data/questions.json");
+
             heroTexture = Content.Load<Texture2D>("hero");
             _hero = new Hero(100, heroTexture, new Vector2(Globals.WindowSize.X /2 , Globals.WindowSize.Y /2));
 
@@ -70,7 +72,12 @@ namespace Recource_Collection
             _questionCreator = new QuestionCreator(Window,textBox,_font,new Rectangle((int)_hero.Position.X - textBox.Width, (int)_hero.Position.Y + 100, 300, 50),_cursorTexture, new Vector2((int)_hero.Position.X - textBox.Width, (int)_hero.Position.Y + 100));
 
             _questions = new List<Question>();
-            _questions = QuestionManager.LoadQuestions("Content/Data/questions.json");
+            if (_questions.Count > 0)
+            {
+                _currentQuestion = _questions[0];
+            }
+           
+            
 
 
         }
@@ -87,14 +94,7 @@ namespace Recource_Collection
             _questionCreator.Update();
 
             
-            //if(WasKeyPressed(Keys.A))
-            //{
-            //    CurrentQuestionIndex = PositiveMod(++CurrentQuestionIndex, _questions.Count);
-            //}
-            //if (WasKeyPressed(Keys.D))
-            //{
-            //    CurrentQuestionIndex = PositiveMod(--CurrentQuestionIndex, _questions.Count);
-            //}
+            
 
 
             // TODO: Add your update logic here
--- Recource Collection/QuestionCreator.cs
+++ Recource Collection/QuestionCreator.cs
@@ -3,7 +3,10 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Formats.Asn1;
+using System.IO.Pipes;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
@@ -19,11 +22,21 @@ namespace Recource_Collection
         private StringBuilder _inputBuilder = new StringBuilder();
         private int offset = 10;
         private string answer;
+        private bool drawQuestionBox = false;
+        KeyboardState previousKeyBoardState;
+
+        public List<Question> _questions;
+
+        public Question _currentQuestion;
+        public int CurrentQuestionIndex = 0;
+        private Random rnd = new Random();
+
 
         private bool _hasFocus = false;
         private static GameWindow _window;
         
         private MouseState _previousMouseState;
+        public int HandleCursorPos() => (int)_font.MeasureString(_inputBuilder.ToString()).X;
 
         public QuestionCreator(GameWindow window, Texture2D textBoxTexture, SpriteFont font, Rectangle textBoxRect, Texture2D cursorTexture,Vector2 cursorPosition)
         {
@@ -53,16 +66,31 @@ namespace Recource_Collection
                 }
             }
         }
+        public void NextQuestion()
+        {
+            CurrentQuestionIndex++;
+            if (CurrentQuestionIndex >= _questions.Count)
+            {
+                CurrentQuestionIndex = 0;
+            }
+            _currentQuestion = _questions[CurrentQuestionIndex];
+        }
+
+        public void CheckAnswer(string answer , string question)
+        {
+            if (answer == question)
+            {
+                drawQuestionBox = false;
+            }
+        }
 
         private void HandleLength()
         {
-            if(_inputBuilder.Length > Globals.MaxChars)
+            if (_inputBuilder.Length > Globals.MaxChars)
             {
                 _inputBuilder.Length = Globals.MaxChars;
             }
         }
-
-        public int HandleCursorPos() => (int)_font.MeasureString(_inputBuilder.ToString()).X;
         
 
         private void OnTextInput(object sender, TextInputEventArgs e)
@@ -76,6 +104,20 @@ namespace Recource_Collection
                         _inputBuilder.Remove(_inputBuilder.Length - 1, 1);
                     break;
 
+                case '\r':
+                case '\n':
+                    answer = _inputBuilder.ToString();
+                    _inputBuilder.Length = 0;
+                    if (answer.Equals(_currentQuestion.AnswerTxt))
+                    {
+                        Debug.WriteLine("Correct , sigma!");
+                        NextQuestion();
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Wrong , diddy blud!");
+                    }
+                    break;
                 default:
                     _inputBuilder.Append(c);
                     break;
@@ -87,13 +129,27 @@ namespace Recource_Collection
         public void Update()
         {
             var mouseState = Mouse.GetState();
+            KeyboardState keyboardState = Keyboard.GetState();
             if (_previousMouseState.LeftButton == ButtonState.Released &&
                 mouseState.LeftButton == ButtonState.Pressed)
             {
                 HandleClick(mouseState.Position);
             }
+            if (keyboardState.IsKeyDown(Keys.Tab) && previousKeyBoardState.IsKeyUp(Keys.Tab))
+            {
+                drawQuestionBox = !drawQuestionBox;
+            }
 
+            if (keyboardState.IsKeyDown(Keys.Space) && !previousKeyBoardState.IsKeyDown(Keys.Space))
+            {
+                if (_questions.Count > 0)
+                {
+                    int randomIndex = rnd.Next(0, _questions.Count); 
+                    _currentQuestion = _questions[randomIndex];          
+                }
+            }
 
+            previousKeyBoardState = keyboardState;
             _previousMouseState = mouseState;
             HandleLength();
         }
@@ -101,10 +157,20 @@ namespace Recource_Collection
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(_textBoxTexture, _textBoxRect, Color.White);
-            spriteBatch.DrawString(_font, $"Focus: {_hasFocus}", new Vector2(_textBoxRect.X, _textBoxRect.Y - 30), Color.Yellow);
-            spriteBatch.Draw(_cursorTexture, new Rectangle((int)HandleCursorPos() + _textBoxRect.X + offset, (int)_cursorPostion.Y, 4, 30), Color.Black);
-            spriteBatch.DrawString(_font, _inputBuilder.ToString(), new Vector2(_textBoxRect.X + 10, _textBoxRect.Y + 10), Color.Red);
+
+            if (drawQuestionBox)
+            {
+                if (_currentQuestion != null)
+                {
+                    spriteBatch.DrawString(_font, _currentQuestion.QuestionsTxt, new Vector2(50, 100), Color.Black);
+                }
+
+                spriteBatch.Draw(_textBoxTexture, _textBoxRect, Color.White);
+                spriteBatch.DrawString(_font, $"Focus: {_hasFocus}", new Vector2(_textBoxRect.X, _textBoxRect.Y - 30), Color.Yellow);
+                spriteBatch.Draw(_cursorTexture, new Rectangle((int)HandleCursorPos() + _textBoxRect.X + offset, (int)_cursorPostion.Y, 4, 30), Color.Black);
+                spriteBatch.DrawString(_font, _inputBuilder.ToString(), new Vector2(_textBoxRect.X + 10, _textBoxRect.Y + 10), Color.Red);
+            }
+            
         }
 
         public string GetInputText() => _inputBuilder.ToString();
